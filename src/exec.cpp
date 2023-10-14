d6c4f5eb9816a4c616482abeed68def8315669fb
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2023-10-14 00:44:34 -0300

    test: add exec function utility

---
+++ src/exec.cpp
@@ -0,0 +1,18 @@
+#include <cstdarg>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+std::string exec(const char* cmd) {
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) return "ERROR";
+    char buffer[128];
+    std::string result = "";
+    while (!feof(pipe.get())) {
+        if (fgets(buffer, 128, pipe.get()) != NULL)
+            result += buffer;
+    }
+    return result;
+}
