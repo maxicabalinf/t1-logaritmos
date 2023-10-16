d40ce6749f41b6b5fccd169a5d54ea3f9b7008e9
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2023-10-16 18:19:46 -0300

    refactor(r_tree): change int for long for arrays

--- src/r_tree.cpp
+++ src/r_tree.cpp
@@ -9,7 +9,7 @@
 
 Node::Node() {}
 
-Node::Node(MaxChildrenArray<Rectangle> keys, MaxChildrenArray<int> children, int keys_qty, bool is_leaf) {
+Node::Node(MaxChildrenArray<Rectangle> keys, MaxChildrenArray<long int> children, int keys_qty, bool is_leaf) {
     this->keys = keys;
     this->children = children;
     this->keys_qty = keys_qty;
@@ -18,19 +18,19 @@ Node::Node(MaxChildrenArray<Rectangle> keys, MaxChildrenArray<int> children, int
 
 Node Node::toNode(Rectangle rectangle) {
     MaxChildrenArray<Rectangle> keys = {rectangle};
-    MaxChildrenArray<int> children = {0};
+    MaxChildrenArray<long int> children = {0};
     return Node(keys, children, 1, true);
 }
 
 template <typename T>
-Node Node::readNode(T &file, int index) {
+Node Node::readNode(T &file, long int index) {
     Node extracted_node;
     file.seekg(index * sizeof(Node));
     file.read((char *)&extracted_node, sizeof(Node));
     return extracted_node;
 }
-template Node Node::readNode(fstream &file, int index);
-template Node Node::readNode(ifstream &file, int index);
+template Node Node::readNode(fstream &file, long int index);
+template Node Node::readNode(ifstream &file, long int index);
 
 string Node::toText(void) {
     string node_text;
@@ -52,3 +52,23 @@ string Node::toText(void) {
 
     return node_text;
 }
+
+/**
+ * @brief Compara dos asociaciones centro-nodo-rectangulo para ordenar ascendentemente (eje X).
+ *
+ * @return true
+ * @return false
+ */
+bool sort_by_X(const tuple<Point, Node, Rectangle>& a, const tuple<Point, Node, Rectangle>& b) {
+    return get<0>(a)[0] < get<0>(b)[0];
+};
+
+/**
+ * @brief Compara dos asociaciones centro-nodo-rectangulo para ordenar ascendentemente (eje Y).
+ *
+ * @return true
+ * @return false
+ */
+bool sort_by_Y(const tuple<Point, Node, Rectangle>& a, const tuple<Point, Node, Rectangle>& b) {
+    return get<0>(a)[1] < get<0>(b)[1];
+};
--- src/r_tree.h
+++ src/r_tree.h
@@ -6,36 +6,47 @@
 
 using namespace std;
 
-#define MAX_CHILDREN 5
+// #define MAX_CHILDREN 204 /* PARA CLUSTER DE 4096 BYTES. */
+#define MAX_CHILDREN 10 /* PARA CLUSTER DE 4096 BYTES. */
 
-typedef array<int, 4> Rectangle; /* Representación de un rectángulo como <xmin, ymin, xmax, ymax> */
+
+typedef array<double, 2> Point;
+
+typedef array<long int, 4> Rectangle; /* Representación de un rectángulo como <xmin, ymin, xmax, ymax> */
 
 template <typename T>
 using MaxChildrenArray = array<T, MAX_CHILDREN>;
 
 /**
  * @brief Representación de un nodo del árbol R.
+ Tamaño nodo: 20 * MAX_CHILDREN + 8 bytes
  *
  */
 class Node {
    public:
     MaxChildrenArray<Rectangle> keys;
-    MaxChildrenArray<int> children;
+    MaxChildrenArray<long int> children;
     int keys_qty = 0;
     bool is_leaf = false;
 
     Node(void);
 
-    Node(MaxChildrenArray<Rectangle> keys, MaxChildrenArray<int> children, int keys_qty, bool is_leaf = false);
+    Node(MaxChildrenArray<Rectangle> keys, MaxChildrenArray<long int> children, int keys_qty, bool is_leaf = false);
 
     static void r_tree_nearest_X(vector<Rectangle> initial_associated_rectangles, int M, string file_name);
 
     static void r_tree_sort_tile_recursive(vector<Rectangle> initial_associated_rectangles, int M, string file_name);
 
+    static void r_tree_hilbert(vector<Rectangle> initial_associated_rectangles, int M, string file_name);
+
     static Node toNode(Rectangle rectangle);
 
     template <typename T>
-    static Node readNode(T& file, int index);
+    static Node readNode(T& file, long int index);
 
     string toText(void);
 };
+
+bool sort_by_X(const tuple<Point, Node, Rectangle>& a, const tuple<Point, Node, Rectangle>& b);
+
+bool sort_by_Y(const tuple<Point, Node, Rectangle>& a, const tuple<Point, Node, Rectangle>& b);
